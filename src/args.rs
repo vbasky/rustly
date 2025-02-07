67e733c7b233e53eaa185017ad8b425271543237
Vikram Bhaskaran <vikram.bhaskaran@outlook.com>
2025-02-08 08:35:08 +1100

    Reorganising files

--- src/args.rs
+++
@@ -1,9 +0,0 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-#[command(author, version, about, long_about = None)]
-pub struct Rustly {
-    /// Input options
-    #[arg(short, long)]
-    pub input: String,
-}
