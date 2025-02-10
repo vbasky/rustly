29d494173f2cb056759e190cb0ad93af8c5f2619
Vikram Bhaskaran <vikram.bhaskaran@outlook.com>
2025-02-11 09:28:05 +1100

    Modified the palindrome trait

--- src/iterators/palindrome.rs
+++ src/iterators/palindrome.rs
@@ -1,22 +1,24 @@
 pub trait Palindrome {
-    fn is_palindrome(&self) -> bool;
+    fn is_palindrome(val: impl Into<String>) -> bool;
 }
 
 impl Palindrome for String {
-    fn is_palindrome(&self) -> bool {
-        self.to_lowercase().chars().eq(self.chars().rev())
+    fn is_palindrome(val: impl Into<String>) -> bool {
+        let val: String = val.into();
+        val.to_lowercase().chars().eq(val.chars().rev())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+
+    use super::Palindrome;
 
     #[test]
     fn test_is_palindrome() {
-        assert!("".to_string().is_palindrome());
-        assert!("a".to_string().is_palindrome());
-        assert!("madam".to_string().is_palindrome());
-        assert!("racecar".to_string().is_palindrome());
+        assert!(String::is_palindrome("".to_string()));
+        assert!(String::is_palindrome(String::from("a")));
+        assert!(String::is_palindrome("madam"));
+        assert!(String::is_palindrome("racecar"));
     }
 }
